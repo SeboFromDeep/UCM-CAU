e7d69f14d6d5669e6b0dfb783a491611679f5d3a
SeboFromDeep <sebashoyomavs@gmail.com>
2022-11-21 21:30:43 +0100

    changes

--- validators/users.js
+++ validators/users.js
@@ -1,12 +1,14 @@
 const {check, validationResult} = require("express-validator")
 
-// TODO: validar que la contraseña sea alfanumérica, y el formato del número de empleado
+// TODO: validar que la contraseña sea alfanumérica, y el formato del número de empleado(solo si esta activado el checkbox)
 const validateSignup = [
     check("email", "Introduce tu correo UCM")
     .custom((value, {req}) => {
         if (!value.endsWith("@ucm.es")) throw new Error("Introduce tu correo UCM")
         return true
     }),
+    check("username", "Introduce tu Nombre de Usuario")
+    .notEmpty(),
     check("password")
     .isLength({min: 8, max:16}).withMessage("La contraseña debe tener entre 8 y 16 caracteres"),
     check("password-confirmation")
