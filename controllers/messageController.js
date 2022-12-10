fb21651a05857155868fa708061de7f98bf1ff09
CarloDubini <45142126+CarloDubini@users.noreply.github.com>
2022-12-10 14:21:18 +0100

    he editado  algunos avisos del database

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -54,7 +54,7 @@ class messagesController {
                         messageDAO.getHistoricMessageUser(user.userID,
                             (error, messages) => {
                                 if (error) res.json(error)
-                                else res.status(200).render("userMainPage", {messages: messages, current: ".his-avisos"})
+                                else res.status(200).render("userMainPage", {messages: messages, current: ".historico-de-avisos"})
                             }
                         )
                     }
@@ -62,7 +62,7 @@ class messagesController {
                         messageDAO.getMyHistoricMessagesTecnico(user.userID,
                             (error, messages) => {
                                 if (error) res.json(error)
-                                else res.status(200).render("technicianMainPage", {messages: messages, current: ".his-avisos"})
+                                else res.status(200).render("technicianMainPage", {messages: messages, current: ".historico-de-avisos"})
                             }
                         )
                     }
